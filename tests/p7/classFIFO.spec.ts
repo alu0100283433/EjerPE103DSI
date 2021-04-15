073eb0e9b59e30993ab832e0368ab15e9f6c3630
Manuel Ramon Regalado Peraza <alu0100283433@ull.edu.es>
2021-04-15 10:10:19 +0100

    Test push() de Fifo : Fallo.

--- tests/p7/classFIFO.spec.ts
+++ tests/p7/classFIFO.spec.ts
@@ -11,4 +11,10 @@ describe('bloque tests clase Fifo', () => {
   it('- Nº de elementos de la cola actualmente es 0', () => {
     expect(cola.size()).to.be.equal(0);
   });
+
+  it('- Con un push() ahora la cola tiene tamaño 1', () => {
+    cola.push(3);
+
+    expect(cola.size()).to.be.equal(1);
+  });
 });
