8d99cc88b4360933312f6258192b7af3bf0bad91
Manuel Ramon Regalado Peraza <alu0100283433@ull.edu.es>
2021-04-15 11:01:27 +0100

    Añadida la documentacion

--- src/p7/classFIFO.ts
+++ src/p7/classFIFO.ts
@@ -1,20 +1,46 @@
 // Clase Fifo. Simula el comportamiento de una cola.
 
+/**
+ * Clase que simula una estructura tipo FIFO. Simulando el comportamiento de
+ * una cola (Queue).
+ * @class
+ */
 export class Fifo {
+  /**
+   * Una estructura tipo Array que simulará una cola con los métodos provistos
+   * de la clase.
+   * @private, @member
+   */
   private estructura: any[];
 
   constructor() {
     this.estructura = [];
   }
 
+  /**
+   * Calcula el tamaño de la estructura FIFO, de cuantos elementos la componen.
+   * @returns Un _number_ que representa el número de elementos la estrutura
+   * FIFO:
+   */
   size(): number {
     return this.estructura.length;
   }
 
+  /**
+   * Añade un elemento nuevo a la estructura FIFO. Los nuevos elementos añadidos
+   * serán insertados/colocados al final de la estructura.
+   * @param dato El tipo de dato que se insertará en la estructura.
+   */
   push(dato: any): void {
     this.estructura.push(dato);
   }
 
+  /**
+   * Devuelve el dato que se encuentra en la primera posición de la estructura
+   * FIFO, llamada _Cabeza_ o _HEAD_. Pero **no lo extrae**.
+   * @returns El dato que se encuentra en la primera posición de la estructura,
+   * en la _Cabeza_ de la misma.
+   */
   pseek(): any {
     if (this.estructura.length > 0) {
       return this.estructura[0];
@@ -23,6 +49,14 @@ export class Fifo {
     }
   }
 
+  /**
+   * Devuelve una cadena formateada con la estructura interna del FIFO. El
+   * primer elemento va acompañado del texto _**(H)**_, indicando que es la
+   * _Cabeza_ o _HEAD_ de la estructura, en cambio el último elemento le
+   * acompaña un _**(T)**_ de _Cola/Tail_. Los elementos van separados por
+   * _**->**_
+   * @returns Una cadena formateada con la estructura del FIFO.
+   */
   print(): string {
     let resultado: string = '';
     const cabeza = 0;
@@ -49,6 +83,11 @@ export class Fifo {
     return resultado;
   }
 
+  /**
+   * Extrae el primer elemento (la _Cabeza_ o _HEAD_) del FIFO/Cola y devuelve
+   * su valor
+   * @returns El valor extraido de la primera posicioón del FIFO.
+   */
   pop(): any {
     return this.estructura.shift();
   }
