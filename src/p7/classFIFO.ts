7940eb4541942111723b520a62f51d1a112928cb
Manuel Ramon Regalado Peraza <alu0100283433@ull.edu.es>
2021-04-15 10:38:44 +0100

    Tests print() de Fifo : Exito.

--- src/p7/classFIFO.ts
+++ src/p7/classFIFO.ts
@@ -22,4 +22,30 @@ export class Fifo {
       return undefined;
     }
   }
+
+  print(): string {
+    let resultado: string = '';
+    const cabeza = 0;
+    const cola = this.estructura.length;
+
+    if (this.estructura.length > 0) {
+      for (let i = 0; i < cola; ++i) {
+        resultado += this.estructura[i];
+
+        if (i == cabeza) {
+          resultado += '(H)';
+        }
+
+        if (i == cola - 1) {
+          resultado += '(T)';
+        }
+
+        if (i < cola - 1) {
+          resultado += '->';
+        }
+      }
+    }
+
+    return resultado;
+  }
 }
